d39976516fa470f5fb59fbb4c2a05de9063a2270
Erin Corson <erin.corson@colorado.edu>
2015-01-02 16:12:00 -0700

    adding tasks for comparing watch price with current price and notifying when the price is <= to watch price

--- code/myapp.py
+++ code/myapp.py
@@ -13,6 +13,18 @@ app.config_from_object(celeryconfig)
 def test_beater():
     return "I'm testing beat!"
 
+@app.task
+def check_price(item):
+    data = get_page(item['url'])
+    if data:
+        price = parse_price(data)
+        if price and price <= item["price"]:
+            notify_watcher.delay(item, price)
+
+@app.task
+def notify_watcher(item, price):
+    return "ALERT! One of the items you are watching has dropped in price. \n{0} now @ ${1}".format(item["name"], price)
+
 #Begin Helper Functions
 
 def get_page(url):
