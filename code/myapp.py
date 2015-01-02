5deded582e919575ea8824e5761e01ab1bc2cbff
Erin Corson <erin.corson@colorado.edu>
2015-01-02 15:53:32 -0700

    adding helper functions

--- code/myapp.py
+++ code/myapp.py
@@ -1,4 +1,5 @@
-#import requests
+import requests
+import re
 #import json
 from celery import Celery
 import celeryconfig
@@ -6,9 +7,22 @@ import celeryconfig
 app = Celery('myapp')
 app.config_from_object(celeryconfig)
 
+#Begin Tasks
+
 @app.task
 def test_beater():
     return "I'm testing beat!"
+
+#Begin Helper Functions
+
+def get_page(url):
+    r = requests.get(url)
+    return r.text if r.ok else ""
+
+def parse_price(data):
+    match = re.search(r'<span id="priceblock_ourprice".*>(.*)</span>', data)
+    return match.group(1) if match else ""
+
 
 if __name__ == '__main__':
     app.start()
